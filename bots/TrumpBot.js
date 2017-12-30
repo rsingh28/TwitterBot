4d3c01468a2e392c34ea448640e2f98d89866160
rsingh28 <rashmeet.singh28@gmail.com>
2017-12-29 23:17:30 -0500

    TrumpBot finished (withou comments)

--- bots/TrumpBot.js
+++ bots/TrumpBot.js
@@ -4,20 +4,18 @@ var Twit = require('twit');
 
 var config = require('../config');
 
-// console.log(config);  --> Sanity Check
+var fs = require('fs');
 
 var T = new Twit(config);
 
-function tweetIt(text){
-	var twitterPost = {
-		status: text
-	}
+function tweetIt(tweet){
 
-	T.post('statuses/update', twitterPost, post); 
+	T.post('statuses/update', tweet, post); 
 
 	function post(err, data, response) {
 		if(err){
 			console.log("Oops! Something went wrong!");
+			console.log(err);
 		}
 		else{
 			console.log("Tweet is up on twitter!");
@@ -25,12 +23,32 @@ function tweetIt(text){
 	}
 }
 
+function apology(text){
+	var pic = fs.readFileSync('../Media/sorryPup.jpg', { encoding: 'base64' });
 
-var stream = T.stream('statuses/filter', { follow: '3106272854' });
+	T.post('media/upload', { media_data: pic }, upload);
+
+	function upload(err, data, response){
+		var id = data.media_id_string;
+
+		var tweet = {
+			status: text,
+			media_ids: [id]
+		}
+
+		tweetIt(tweet);
+	}
+
+}
+
+var stream = T.stream('statuses/filter', { follow: '25073877' });
 
 stream.on('tweet', function (data) {
-	console.log(data.user.screen_name + " posted!");
-
-	tweetIt("@" + data.user.screen_name + " just posted!");
-
+	var twitterId = data.user.screen_name;
+	if(twitterId == 'I_am_aBot'){
+		return;
+	}
+	console.log(twitterId + " posted!");
+	var randomNum = Math.floor(Math.random() * 100);
+	apology("@" + twitterId + " tweeted again :( . I would like to apologize on his behalf #RandomNumber" + randomNum);
 });
